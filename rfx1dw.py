422da9ad5042606558702991997622924093618c
Oleg Volkov <olegv@parallels.com>
2015-11-14 23:35:14 +0300

    Yet another model complication - quadratic growth of small population

--- rfx1dw.py
+++ rfx1dw.py
@@ -3,7 +3,7 @@
 # logistic growth, covered rabbits and hunting saturation
 #
 # H  = c * F * (R - R0) * Rc / (R + Rc)
-# dR = dt * [ a * R * (1 - R/Rs) - H ]
+# dR = dt * [ a * (R * R / (R + R1)) * (1 - R/Rs) - H ]
 # dF = dt * [ d * H - b * F ]
 #
 # a  - rabbit growth
@@ -11,6 +11,7 @@
 # c  - hunting efficiency
 # d  - eaten rabbit to fox born coefficient
 # R0 - covered rabbits population
+# R1 - lonely rabbits threshold
 # Rc - hunting saturation
 # Rs - saturated rabbits population
 #
@@ -21,14 +22,15 @@ a = 1./20
 b = 1./3000
 c = 1./100
 d = 0.005
-R0 = .01
-Rc = 200.
-Rs = 500.
+R0 = .1
+R1 = 1.
+Rc = 50.
+Rs = 100.
 
 # Equilibrium
 R_ = b / (c * d)
 R_e = Rc * (R0 + R_) / (Rc - R_)
-F_e = (a / c) * R_e * (1 - R_e/Rs) * (R_e + Rc) / ((R_e - R0) * Rc)
+F_e = (a / c) * R_e * R_e * (1 - R_e/Rs) * (R_e + Rc) / ((R_e + R1) * (R_e - R0) * Rc)
 
 # Time quantum
 dt = .1
@@ -44,7 +46,7 @@ def migration_rate(P):
 def population_up(R, F):
 	catched = c * F * (R - R0) * Rc / (R + Rc)
 	born = d * catched
-	vR = a * R * (1 - R/Rs) - catched + .2 * migration_rate(R)
+	vR = a * R * R * (1 - R/Rs) / (R + R1) - catched + .1 * migration_rate(R)
 	vF = born - b * F + .1 * migration_rate(F)
 	return R + vR * dt, F + vF * dt
 
